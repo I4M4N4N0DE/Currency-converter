c5c1ced4bb3ad11ae613103ed90929492f6a1007
Renny <100791149+I4M4N4N0DE@users.noreply.github.com>
2022-10-20 20:58:22 +0200

    Update CurrencyConverter.py

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,26 +1,24 @@
-import tkinter as tk
-from tkinter import *
+import tkinter as tkin
     
-def main(rate):
+def main(rate, money):
             
         cash = float(input("Insert your money: "))
         resum = cash * rate
         sresum = str(resum)
-        print(sresum)
+        print(sresum + money)
 
 def Gui():
     
-        buttonwin = tk.Tk()
+        buttonwin = tkin.Tk()
         name = buttonwin.title("EXCHANGE")
         buttonwin.geometry("200x200")
     
-        b1 = tk.Button(buttonwin, text="CZK -> $", command=lambda:main(0.039804)).place(x=10, y=20)
-        b2 = tk.Button(buttonwin, text="$ --> CZK", command=lambda:main(24.512)).place(x=110, y=20)
-        b3 = tk.Button(buttonwin, text="CZK --> €", command=lambda:main(24.485)).place(x=10, y=80)
-        b4 = tk.Button(buttonwin, text="€ --> CZK", command=lambda:main(0.40841)).place(x=110, y=80)
-        b5 = tk.Button(buttonwin, text="$ --> €", command=lambda:main(1.008)).place(x=18, y=130)
-        b6 = tk.Button(buttonwin, text="€ --> $", command=lambda:main(0.998898)).place(x=118, y=130)
-        b7 = tk.Button(buttonwin, text="Exit", command=buttonwin.destroy).place(x=75, y=165)
-        buttonwin.mainloop()
+        b1 = tkin.Button(buttonwin, text="CZK -> $", command=lambda:main(0.039804, " $")).place(x=10, y=20) # here you can change the actual exchange rate for this current exchange (lambda:main(current_rate))
+        b2 = tkin.Button(buttonwin, text="$ --> CZK", command=lambda:main(24.512, " CZK")).place(x=110, y=20) # here you can change the actual exchange rate for this current exchange (lambda:main(current_rate))
+        b3 = tkin.Button(buttonwin, text="CZK --> €", command=lambda:main(24.485, " €")).place(x=10, y=80) # here you can change the actual exchange rate for this current exchange (lambda:main(current_rate))
+        b4 = tkin.Button(buttonwin, text="€ --> CZK", command=lambda:main(0.40841, " CZK")).place(x=110, y=80) # here you can change the actual exchange rate for this current exchange (lambda:main(current_rate))
+        b5 = tkin.Button(buttonwin, text="$ --> €", command=lambda:main(1.008, " €")).place(x=18, y=130) # here you can change the actual exchange rate for this current exchange (lambda:main(current_rate))
+        b6 = tkin.Button(buttonwin, text="€ --> $", command=lambda:main(0.998898, " $")).place(x=118, y=130) # here you can change the actual exchange rate for this current exchange (lambda:main(current_rate))
+        b7 = tkin.Button(buttonwin, text="Exit", command=buttonwin.destroy).place(x=75, y=165) # here you can change the actual exchange rate for this current exchange (lambda:main(current_rate))
     
 Gui()
