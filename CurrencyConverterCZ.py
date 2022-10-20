5fb1b6e96e61566b1b3224237134bbd802065c87
Renny <100791149+I4M4N4N0DE@users.noreply.github.com>
2022-10-20 20:58:05 +0200

    Update CurrencyConverterCZ.py

--- CurrencyConverterCZ.py
+++ CurrencyConverterCZ.py
@@ -1,11 +1,11 @@
 import tkinter as tkin
     
-def main(rate):
+def main(rate, money):
             
         cash = float(input("Zadejte částku: "))
         resum = cash * rate
         sresum = str(resum)
-        print(sresum)
+        print(sresum + money)
 
 def Gui():
     
@@ -13,12 +13,12 @@ def Gui():
         name = buttonwin.title("SMĚNA")
         buttonwin.geometry("200x200")
     
-        b1 = tkin.Button(buttonwin, text="CZK -> $", command=lambda:main(0.039804)).place(x=10, y=20) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
-        b2 = tkin.Button(buttonwin, text="$ --> CZK", command=lambda:main(24.512)).place(x=110, y=20) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
-        b3 = tkin.Button(buttonwin, text="CZK --> €", command=lambda:main(24.485)).place(x=10, y=80) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
-        b4 = tkin.Button(buttonwin, text="€ --> CZK", command=lambda:main(0.40841)).place(x=110, y=80) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
-        b5 = tkin.Button(buttonwin, text="$ --> €", command=lambda:main(1.008)).place(x=18, y=130) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
-        b6 = tkin.Button(buttonwin, text="€ --> $", command=lambda:main(0.998898)).place(x=118, y=130) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
+        b1 = tkin.Button(buttonwin, text="CZK -> $", command=lambda:main(0.039804, " $")).place(x=10, y=20) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
+        b2 = tkin.Button(buttonwin, text="$ --> CZK", command=lambda:main(24.512, " CZK")).place(x=110, y=20) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
+        b3 = tkin.Button(buttonwin, text="CZK --> €", command=lambda:main(24.485, " €")).place(x=10, y=80) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
+        b4 = tkin.Button(buttonwin, text="€ --> CZK", command=lambda:main(0.40841, " CZK")).place(x=110, y=80) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
+        b5 = tkin.Button(buttonwin, text="$ --> €", command=lambda:main(1.008, " €")).place(x=18, y=130) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
+        b6 = tkin.Button(buttonwin, text="€ --> $", command=lambda:main(0.99889, " $")).place(x=118, y=130) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
         b7 = tkin.Button(buttonwin, text="Exit", command=buttonwin.destroy).place(x=75, y=165) # zde si můžete změnit aktuální měnový kurz pro daný převod (lambda:main(aktuální kurz v desetinném zápise))
         buttonwin.mainloop()
     
